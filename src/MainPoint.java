983c33b6757218f8cf006e1faf87518cd92c8560
Dmitri Mandaji <badmopss@gmail.com>
2023-04-01 13:31:37 +0200

    repair class MainPoint

--- src/MainPoint.java
+++ src/MainPoint.java
@@ -3,14 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainPoint {
+
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    System.out.print("Координаты точки x: ");
-    int x = Integer.parseInt(br.readLine().toUpperCase());
-
-    System.out.print("Координаты точки y: ");
-    int y = Integer.parseInt(br.readLine().toUpperCase());
+    Point point = Point.read(br);
+    String quadrant = point.getQuadrant();
+    System.out.println(quadrant);
   }
 }
-
